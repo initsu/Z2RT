a50f6b50113e894dba06d6f9a7a958f54814f4e5
initsu <init.public@gmail.com>
2025-02-09 01:38:21 +0100

    feat: save state to localStorage so you can refresh without losing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ALL_CHECKOFFS } from './Data';
 import { OverworldCard } from './OverworldCard';
 import { PalaceCard } from './PalaceCard';
 import { PopoutButton } from './PopoutButton';
 
+const LOCAL_STORAGE_KEY = "Z2RT_stateArray";
+
 function App() {
-  const [stateArray, setStateArray] = useState<boolean[]>(
-    Array(ALL_CHECKOFFS.length).fill(false)
-  );
+  const [stateArray, setStateArray] = useState<boolean[]>(() => {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (saved) {
+      const parsed = JSON.parse(saved);
+      if (Array.isArray(parsed)) {
+        if (parsed.length == ALL_CHECKOFFS.length) {
+          return parsed;
+        }
+      }
+    }
+    return Array(ALL_CHECKOFFS.length).fill(false);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(stateArray));
+  }, [stateArray]);
 
   const toggleState = (index: number) => {
     setStateArray((prev) =>
